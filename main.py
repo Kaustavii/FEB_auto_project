c5bb376f000ee7b14b57fc938f0e0c2694b4cffa
Kaustavi Mukherjee <mkkaustavi@visitor-10-58-231-56.wifi.berkeley.edu>
2025-09-25 15:07:00 -0700

    Your message

--- main.py
+++ main.py
@@ -13,8 +13,24 @@ def controller(x):
         ndarray: numpy array of shape (2,) containing [fwd acceleration, steering rate]
     """
     ... # YOUR CODE HERE
-
+def controller(x):
+    """
+    Minimal controller to see the car move in the simulator.
+    """
+    # Constant forward acceleration (safe)
+    a = 1.0
+    
+    # Small constant steering rate (so the car wiggles a bit)
+    phi_dot = 0
+    
+    return np.array([a, phi_dot]) 
+def lookahead(S):
+    """set lookahead distance to aim car to go to"""
+    lookahead = centerline(S)
+    return lookahead
 sim.set_controller(controller)
+lookahead(0.1)
 sim.run()
 sim.animate()
 sim.plot()
+
